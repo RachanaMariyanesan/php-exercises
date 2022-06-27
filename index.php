b8f46cc61f87e1de14cbb5ec16d263bd0cf04642
RachanaMariyanesan <rachana23052002@gmail.com>
2022-06-27 14:29:07 +0530

    Add style

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>index</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+<div class="container bg-dark">
+<form class=" text-white">
+  <h1 class="text-center">Student Registration</h1>
+  <hr>
+  <div class="form-group" style="margin-bottom:20px">
+    <label for="exampleInputEmail1">Student ID</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Student ID">
+ 
+  </div>
+  <div class="form-group" style="margin-bottom:20px">
+    <label for="exampleInputEmail1">First name</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter First name">
+ 
+  </div>
+  <div class="form-group" style="margin-bottom:20px">
+    <label for="exampleInputEmail1">Last name</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Last name">
+ 
+  </div>
+  <div class="form-group" style="margin-bottom:20px">
+    <label for="exampleInputEmail1">Date of Birth</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Date of Birth">
+ 
+  </div>
+  <div class="form-group" style="margin-bottom:20px">
+    <label for="exampleInputEmail1">Email</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Email">
+ 
+  </div>
+  <div class="form-group" style="margin-bottom:20px">
+    <label for="exampleInputEmail1">Department</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Department">
+ 
+  </div>
+ 
+ 
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form><br>
+</div>
+<script src="https://kit.fontawesome.com/ab02c285ab.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
